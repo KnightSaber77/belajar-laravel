caffc045a9d6a179cee88033f045ddeff984ff2a
josuamustiko <j.aditya.mustiko@gmail.com>
2018-01-10 16:30:44 +0700

    Add Admin Migration Add Provider Migration Add Product Migration Add Payment Migration Add Transaction Migration Delete Unused Migration

---
+++ database/migrations/2018_01_10_080622_create_product_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->string('productname')->unique();
+            $table->integer('providerid')->unsigned();
+            $table->integer('price');
+            $table->tinyinteger('tipe');
+            $table->timestamps();
+
+//            $table->primary('id');
+            $table->foreign('providerid')->references('id')->on('providers');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
--- database/migrations/2018_01_10_081356_create_payment_table.php
+++ database/migrations/2018_01_10_081356_create_payment_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePasswordResetsTable extends Migration
+class CreatePaymentTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,13 @@ class CreatePasswordResetsTable extends Migration
      */
     public function up()
     {
-        Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->index();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        Schema::create('payments', function (Blueprint $table) {
+            $table->string('paymentid')->unique();
+            $table->tinyinteger('status');
+            $table->integer('total');
+            $table->timestamps();
+
+//            $table->primary('paymentid');
         });
     }
 
@@ -27,6 +30,6 @@ class CreatePasswordResetsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('password_resets');
+        Schema::dropIfExists('payments');
     }
 }
