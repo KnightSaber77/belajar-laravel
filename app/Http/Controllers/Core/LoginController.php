35444b5c7e74b3313fa064f3790e1e6e381d5144
josuamustiko <j.aditya.mustiko@gmail.com>
2018-01-15 14:12:47 +0700

    Implemented Middleware and session

--- app/Http/Controllers/Core/LoginController.php
+++ app/Http/Controllers/Core/LoginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Core;
 use App\Core\Services\LoginService;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Session;
 
 class LoginController extends Controller
 {
@@ -24,7 +25,15 @@ class LoginController extends Controller
         $username = $request->input('username');
         $password = $request->input('password');
 
+
         $isAdminExist = $this->loginService->login($username, $password);
-        dd($isAdminExist);
+        if (empty($isAdminExist)) return redirect('/admin');
+        return redirect ('/admin/dashboard');
+    }
+
+    public function logout()
+    {
+        Session::flush();
+        return redirect ('/admin');
     }
 }
---
+++ app/Http/Middleware/AdminLoginMiddleware.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class AdminLoginMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $value = $request->session()->get('username');
+        if (empty($value)) {
+            return redirect('/admin');
+        }
+        return $next($request);
+    }
+}
